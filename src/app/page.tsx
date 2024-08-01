61391abd5bd84c5874cb2c8a3f1771bf945d50c0
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-08-01 16:16:03 +0530

    feat: render mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/a01c81cd-cbf8-4cdd-bc6b-8d25b02f5b7e-bsk3dw.webp",
+    "https://utfs.io/f/28cfc6b0-ebe1-42ac-95ab-f90b4cecb936-1t6f.svg.png"
+  ]
+
+  const mockImages = mockUrls.map((url, index) => {
+    return {
+      url,
+      id: index
+    }
+  })
   return (
     <main >
-      Gallery
+      <div className="flex flex-wrap gap-4 p-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id}>
+            <Image src={image.url} alt="image" width={200} height={200} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
