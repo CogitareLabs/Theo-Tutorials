44a7f2d0530f64564927aaad5115cc0c7928e1c4
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-08-02 10:50:05 +0530

    update: force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { getMyImages } from "~/server/queries";
 import Image from "next/image";
-
+export const dynamic = "force-dynamic";
 export default async function HomePage() {
   const mockUrls = [
     "https://utfs.io/f/a01c81cd-cbf8-4cdd-bc6b-8d25b02f5b7e-bsk3dw.webp",
@@ -20,6 +20,11 @@ export default async function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4 p-4">
+        {posts.map((post) => (
+          <div key={post.id}>
+            {post.name}
+          </div>
+        ))}
         {[...mockImages].map((image, index) => (
           <div key={image.id}>
             <Image src={image.url} alt="image" width={200} height={200} />
