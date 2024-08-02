8ab2d78edcc2bfff6c7753e5b0d29e0b62b5b073
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-08-02 13:18:00 +0530

    connect images to users; server-only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@ import Image from "next/image";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 export default async function HomePage() {
-  const images = await getMyImages();
-  console.log("images: ", images);
-
   return (
     <main>
       <SignedOut>
         <p className="text-center">Please sign in above to view your images</p>
       </SignedOut>
       <SignedIn>
-        <div className="flex flex-wrap gap-4 p-4">
-          {images.map((image, index) => (
-            <div key={image.id} className="flex flex-col gap-2">
-              <Image src={image.url} alt="image" width={200} height={200} />
-              <p>{image.name}</p>
-            </div>
-          ))}
-        </div>
+        <Images />
       </SignedIn>
     </main>
   );
+}
+
+async function Images() {
+  const images = await getMyImages();
+  console.log("images: ", images);
+  return (<div className="flex flex-wrap gap-4 p-4">
+    {images.map((image, index) => (
+      <div key={image.id} className="flex flex-col gap-2">
+        <Image src={image.url} alt="image" width={200} height={200} />
+        <p>{image.name}</p>
+      </div>
+    ))}
+  </div>)
 }
